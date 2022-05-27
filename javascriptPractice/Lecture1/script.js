380d1fecc1e9e010b1bb6a7f245c19776ee1dbec
sulemanwains <104390891+sulemanwains@users.noreply.github.com>
2022-05-27 00:29:00 -0500

    Done with Lab 2 and Lab3
    Done with Lab 2 and Lab3

---
+++ javascriptPractice/Lecture1/script.js
@@ -0,0 +1,76 @@
+let a=["apple", "banana", "pear"];
+
+// let result=[];
+
+// for (let index = 0; index < a.length; index++) {
+//     result.push(a[index].length);
+// }
+
+// console.log(result);
+
+
+
+
+// let result=function pushing(a) {
+// let newfunc=[];    
+// for (let index = 0; index < a.length; index++) {
+//     newfunc.push(a[index].length);
+// }
+
+// return newfunc;
+
+// }
+
+// console.log(result(a));
+
+
+
+
+// let result=[];
+// for (let index = 0; index < a.length; index++) {
+   
+
+//     if ( a[index].length >= 5) {
+
+//         result.push(a[index]);
+        
+//     }
+    
+// }
+// console.log(result);
+
+
+
+
+
+// let result= function find(a){
+
+// for (let index = 0; index < a.length; index++) {
+   
+
+//     if ( a[index].length === 5) {
+        
+//         return a[index];
+//     }
+// }
+// }
+
+// console.log(result(a));
+
+
+
+
+
+// let result= function findIndex(a){
+
+//     for (let index = 0; index < a.length; index++) {
+       
+    
+//         if ( a[index].length === 5) {
+            
+//             return index;
+//         }
+//     }
+//     }
+
+//     console.log(result(a));
